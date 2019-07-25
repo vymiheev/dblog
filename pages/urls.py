a9c4faf15e24d3d697824357fef1ea8a41759866
vymiheev <vymiheev@gmail.com>
2019-07-25 08:05:52 +0300

    initial

---
+++ pages/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+
+from pages import views
+
+urlpatterns = [
+    path('', views.IndexView.as_view()),
+]
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+
+
+class IndexView(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'page.html', {'message': 'hello'})
